df0ae678d3d8112e995800c9af388eb5c4e41ae2
Remigiusz98 <35955156+Remigiusz98@users.noreply.github.com>
2019-04-30 09:06:18 +0200

    auth test

--- src/main/java/com/Remigiusz/MacronutrientsApiREST/Controllers/TestRestAPIs.java
+++ src/main/java/com/Remigiusz/MacronutrientsApiREST/Controllers/TestRestAPIs.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
+/*testtest*/
 public class TestRestAPIs {
 	
 	@GetMapping("/source/user")
